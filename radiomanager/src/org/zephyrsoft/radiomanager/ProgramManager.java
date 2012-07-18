5ebd6b4b14fadfb534c1b8bb3a3d1aef941eb61c
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2012-07-18 20:29:55 +0000

    added method to produce table for the rest of the current day
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/radiomanager/trunk@373 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- radiomanager/src/org/zephyrsoft/radiomanager/ProgramManager.java
+++ radiomanager/src/org/zephyrsoft/radiomanager/ProgramManager.java
@@ -1,6 +1,10 @@
 package org.zephyrsoft.radiomanager;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Manages the broadcast program table and supports the output as HTML.
@@ -12,9 +16,9 @@ public class ProgramManager {
 	private static final String firstColWidth = "5%";
 	private static final String normalColWidth = "10%";
 	
-	private static final String[] weekdays = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
-	private static final String[] hours = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
-		"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" };
+	private static final String[] weekdays = {"Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"}; // monday is first!
+	private static final String[] hours = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+		"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"};
 	
 	/**
 	 * create the webradio program table in HTML
@@ -24,9 +28,9 @@ public class ProgramManager {
 		
 		ret.append("<style type=\"text/css\">\n" + "<!--\n" + ".c1 {\n" + "font-weight:bold;\n" + "font-size:100%;\n"
 			+ "padding:2px;\n" + "border-bottom:1px solid grey;\n" + "border-right:1px solid grey;\n"
-			+ "vertical-align:top;\n" + "}\n" + ".c2 {\n" + "font-weight:normal;\n" + "font-size:90%;\n" + "padding:2px;\n"
-			+ "border-bottom:1px solid grey;\n" + "border-right:1px solid grey;\n" + "vertical-align:top;\n" + "}\n"
-			+ "-->\n" + "</style>\n");
+			+ "vertical-align:top;\n" + "}\n" + ".c2 {\n" + "font-weight:normal;\n" + "font-size:90%;\n"
+			+ "padding:2px;\n" + "border-bottom:1px solid grey;\n" + "border-right:1px solid grey;\n"
+			+ "vertical-align:top;\n" + "}\n" + "-->\n" + "</style>\n");
 		
 		ret.append("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n<tr><td class=\"c1\" style=\"width:"
 			+ firstColWidth + "\">&nbsp;</td>");
@@ -52,6 +56,57 @@ public class ProgramManager {
 		ret.append("</table>");
 		
 		return ret.toString();
+	}
+	
+	public static String getRestOfDayAsHtml(File... radioBaseDirs) {
+		StringBuilder ret = new StringBuilder();
+		
+		ret.append("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
+		GregorianCalendar calendar = new GregorianCalendar();
+		int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+		String currentWeekDay = getWeekdayAsString(calendar.get(Calendar.DAY_OF_WEEK));
+		for (String hour : hours) {
+			int hourAsInt = Integer.parseInt(hour);
+			if (hourAsInt < currentHour) {
+				continue;
+			}
+			ret.append("<tr>");
+			boolean isCurrentHour = hourAsInt == currentHour;
+			ret.append("<td class=\"" + (isCurrentHour ? "c1" : "n1") + "\">" + hour + " Uhr</td>");
+			ret.append("<td class=\"" + (isCurrentHour ? "c2" : "n2") + "\">");
+			BroadcastData broadcastData = CheckForBroadcast.doCheckForBroadcast(currentWeekDay, hour, radioBaseDirs);
+			if (broadcastData.getResultType() == CheckResultEnum.BROADCAST_FOUND) {
+				ret.append(getInfoFileContent(broadcastData.getResultText()));
+			} else {
+				ret.append("&nbsp;");
+			}
+			ret.append("</td>");
+			ret.append("</tr>\n");
+		}
+		ret.append("</table>");
+		
+		return ret.toString();
+	}
+	
+	private static String getWeekdayAsString(int weekdayFromCalendar) {
+		switch (weekdayFromCalendar) {
+			case Calendar.MONDAY:
+				return weekdays[0];
+			case Calendar.TUESDAY:
+				return weekdays[1];
+			case Calendar.WEDNESDAY:
+				return weekdays[2];
+			case Calendar.THURSDAY:
+				return weekdays[3];
+			case Calendar.FRIDAY:
+				return weekdays[4];
+			case Calendar.SATURDAY:
+				return weekdays[5];
+			case Calendar.SUNDAY:
+				return weekdays[6];
+			default:
+				throw new IllegalArgumentException();
+		}
 	}
 	
 	private static String getInfoFileContent(String path) {
