b1be23f8fab14434b8d746272b220277d9c1d299
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2013-11-03 15:30:23 +0000

    eliminate warnings
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/radiomanager/trunk@471 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- src/org/zephyrsoft/radiomanager/ProgramManager.java
+++ src/org/zephyrsoft/radiomanager/ProgramManager.java
@@ -12,26 +12,26 @@ import java.util.GregorianCalendar;
  * @author Mathis Dirksen-Thedens
  */
 public class ProgramManager {
-	
+
 	private static final String firstColWidth = "5%";
 	private static final String normalColWidth = "10%";
-	
-	private static final String[] weekdays = {"Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"}; // monday is first!
-	private static final String[] hours = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
-		"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"};
-	
+
+	private static final String[] weekdays = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" }; // monday is first!
+	private static final String[] hours = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+		"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" };
+
 	/**
 	 * create the webradio program table in HTML
 	 */
 	public static String getProgramTableAsHtml(File... radioBaseDirs) {
 		StringBuilder ret = new StringBuilder();
-		
+
 		ret.append("<style type=\"text/css\">\n" + "<!--\n" + ".c1 {\n" + "font-weight:bold;\n" + "font-size:100%;\n"
 			+ "padding:2px;\n" + "border-bottom:1px solid grey;\n" + "border-right:1px solid grey;\n"
 			+ "vertical-align:top;\n" + "}\n" + ".c2 {\n" + "font-weight:normal;\n" + "font-size:90%;\n"
 			+ "padding:2px;\n" + "border-bottom:1px solid grey;\n" + "border-right:1px solid grey;\n"
 			+ "vertical-align:top;\n" + "}\n" + "-->\n" + "</style>\n");
-		
+
 		ret.append("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n<tr><td class=\"c1\" style=\"width:"
 			+ firstColWidth + "\">&nbsp;</td>");
 		for (String day : weekdays) {
@@ -54,13 +54,13 @@ public class ProgramManager {
 			ret.append("</tr>\n");
 		}
 		ret.append("</table>");
-		
+
 		return ret.toString();
 	}
-	
+
 	public static String getRestOfDayAsHtml(File... radioBaseDirs) {
 		StringBuilder ret = new StringBuilder();
-		
+
 		ret.append("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
 		GregorianCalendar calendar = new GregorianCalendar();
 		int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -111,10 +111,10 @@ public class ProgramManager {
 			}
 		}
 		ret.append("</table>");
-		
+
 		return ret.toString();
 	}
-	
+
 	private static String getWeekdayAsString(int weekdayFromCalendar) {
 		switch (weekdayFromCalendar) {
 			case Calendar.MONDAY:
@@ -135,13 +135,12 @@ public class ProgramManager {
 				throw new IllegalArgumentException();
 		}
 	}
-	
+
 	private static String getInfoFileContent(String path) {
 		String ret = "Sendung ohne Titel";
 		File infoFile = new File(path + File.separator + "info.txt");
 		if (infoFile.exists() && infoFile.canRead()) {
-			try {
-				BufferedReader in = new BufferedReader(new FileReader(infoFile));
+			try (BufferedReader in = new BufferedReader(new FileReader(infoFile))) {
 				StringBuilder content = new StringBuilder((int) infoFile.length());
 				while (in.ready()) {
 					content.append(in.readLine());
@@ -161,7 +160,7 @@ public class ProgramManager {
 		}
 		return ret;
 	}
-	
+
 	public static void main(String[] args) {
 		File[] radioBaseDirs = new File[args.length];
 		if (args != null) {
@@ -173,5 +172,5 @@ public class ProgramManager {
 			Utils.exit(data == null ? 1 : 0);
 		}
 	}
-	
+
 }
